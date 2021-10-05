cbefe33e4249334539f55f7490f8d28ca32443b6
m.sajid <m.sajid@itg.com.kw>
2021-10-05 14:13:49 +0500

    adding testing api route

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
+
+Route::post('/contact', function (Request $request) {
+    return $request->all();
+});
