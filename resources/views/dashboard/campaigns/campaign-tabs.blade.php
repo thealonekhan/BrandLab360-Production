0be1bcc97350c92026e3a1d17abda0a8572d96ce
m.sajid <m.sajid@itg.com.kw>
2022-01-20 12:08:48 +0500

    updating campaign Tabs View Data

--- resources/views/dashboard/campaigns/campaign-tabs.blade.php
+++ resources/views/dashboard/campaigns/campaign-tabs.blade.php
@@ -21,21 +21,23 @@
                     <th>{{$gaAcquisitionCampaign->totalsForAllResults['ga:users']}}</th>
                     <th>{{$gaAcquisitionCampaign->totalsForAllResults['ga:newUsers']}}</th>
                     <th>{{$gaAcquisitionCampaign->totalsForAllResults['ga:sessions']}}</th>
-                    <th>{{$gaAcquisitionCampaign->totalsForAllResults['ga:bounceRate']}}</th>
-                    <th>{{$gaAcquisitionCampaign->totalsForAllResults['ga:avgSessionDuration']}}</th>
+                    <th>{{round($gaAcquisitionCampaign->totalsForAllResults['ga:bounceRate'], 2)}}</th>
+                    <th>{{round($gaAcquisitionCampaign->totalsForAllResults['ga:avgSessionDuration'], 2)}}</th>
                 </tr>
                 </thead>
                 <tbody>
                 @if(!empty($gaAcquisitionCampaign->rows))
                 @foreach($gaAcquisitionCampaign->rows as $campaign)
+                @if($campaign[0] != '(not set)')
                 <tr>
                     <td>{{$campaign[0]}}</td>
                     <td>{{$campaign[5]}}</td>
                     <td>{{$campaign[3]}}</td>
                     <td>{{$campaign[1]}}</td>
-                    <td>{{$campaign[2]}}</td>
-                    <td>{{$campaign[4]}}</td>
+                    <td>{{round($campaign[2], 2)}}</td>
+                    <td>{{round($campaign[4], 2)}}</td>
                 </tr>
+                @endif
                 @endforeach
                 @endif
                 </tbody>
@@ -58,21 +60,23 @@
                     <th>{{$gaAcquisitionSource->totalsForAllResults['ga:users']}}</th>
                     <th>{{$gaAcquisitionSource->totalsForAllResults['ga:newUsers']}}</th>
                     <th>{{$gaAcquisitionSource->totalsForAllResults['ga:sessions']}}</th>
-                    <th>{{$gaAcquisitionSource->totalsForAllResults['ga:bounceRate']}}</th>
-                    <th>{{$gaAcquisitionSource->totalsForAllResults['ga:avgSessionDuration']}}</th>
+                    <th>{{round($gaAcquisitionSource->totalsForAllResults['ga:bounceRate'], 2)}}</th>
+                    <th>{{round($gaAcquisitionSource->totalsForAllResults['ga:avgSessionDuration'], 2)}}</th>
                 </tr>
                 </thead>
                 <tbody>
                 @if(!empty($gaAcquisitionSource->rows))
                 @foreach($gaAcquisitionSource->rows as $source)
+                @if($campaign[0] != '(direct)')
                 <tr>
                     <td>{{$source[0]}}</td>
                     <td>{{$source[5]}}</td>
                     <td>{{$source[3]}}</td>
                     <td>{{$source[1]}}</td>
-                    <td>{{$source[2]}}</td>
-                    <td>{{$source[4]}}</td>
+                    <td>{{round($source[2], 2)}}</td>
+                    <td>{{round($source[4], 2)}}</td>
                 </tr>
+                @endif
                 @endforeach
                 @endif
                 </tbody>
@@ -95,21 +99,23 @@
                     <th>{{$gaAcquisitionMedium->totalsForAllResults['ga:users']}}</th>
                     <th>{{$gaAcquisitionMedium->totalsForAllResults['ga:newUsers']}}</th>
                     <th>{{$gaAcquisitionMedium->totalsForAllResults['ga:sessions']}}</th>
-                    <th>{{$gaAcquisitionMedium->totalsForAllResults['ga:bounceRate']}}</th>
-                    <th>{{$gaAcquisitionMedium->totalsForAllResults['ga:avgSessionDuration']}}</th>
+                    <th>{{round($gaAcquisitionMedium->totalsForAllResults['ga:bounceRate'], 2)}}</th>
+                    <th>{{round($gaAcquisitionMedium->totalsForAllResults['ga:avgSessionDuration'], 2)}}</th>
                 </tr>
                 </thead>
                 <tbody>
                 @if(!empty($gaAcquisitionMedium->rows))
                 @foreach($gaAcquisitionMedium->rows as $medium)
+                @if($campaign[0] != '(none)')
                 <tr>
                     <td>{{$medium[0]}}</td>
                     <td>{{$medium[5]}}</td>
                     <td>{{$medium[3]}}</td>
                     <td>{{$medium[1]}}</td>
-                    <td>{{$medium[2]}}</td>
-                    <td>{{$medium[4]}}</td>
+                    <td>{{round($medium[2], 2)}}</td>
+                    <td>{{round($medium[4], 2)}}</td>
                 </tr>
+                @endif
                 @endforeach
                 @endif
                 </tbody>
